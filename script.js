ea5156ac3b5d1a7cba6aed24994bfb37944c2814
Sammy <76911140+Sam-123-tech@users.noreply.github.com>
2021-01-29 19:19:11 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter user name");
+ var b=prompt("Enter password");
+  	if(a== "Samridhi"&&b=="Tia")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="Index9.html";
+  	}
+  	
